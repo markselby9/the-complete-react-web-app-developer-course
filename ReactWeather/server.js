636758d141898226310771cb2e7f53894034cd9b
冯超逸 <markselbyfcy@gmail.com>
2016-11-17 20:15:01 +0800

    add npm start

--- ReactWeather/server.js
+++ ReactWeather/server.js
@@ -2,9 +2,20 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const port = process.env.PORT || 3000;
+
+// express middleware
+app.use(function (request, response, next) {
+    next();
+    // if (request.headers['x-forwarded-proto'] === 'http') {  //HTTP or HTTPS
+    //     next();
+    // } else {
+    //     response.redirect('http://' + request.hostname + request.url);
+    // }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+app.listen(port, function () {
+    console.log('Express server is up on port 3000');
 });
