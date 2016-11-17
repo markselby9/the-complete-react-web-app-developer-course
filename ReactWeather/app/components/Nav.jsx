4bfd11971b010571c83819229c2fc34f62a6cbb1
冯超逸 <markselbyfcy@gmail.com>
2016-11-17 21:21:16 +0800

    style Nav bar

--- ReactWeather/app/components/Nav.jsx
+++ ReactWeather/app/components/Nav.jsx
@@ -1,13 +1,38 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var NavComponent = (props) => (
-    <div>
-        <h2>Nav</h2>
-        <IndexLink to="/">Weather</IndexLink>
-        <Link to="/about">About</Link>
-        <Link to="/more">More</Link>
-    </div>
+var NavComponent = React.createClass({
+        onSearch: function (e) {
+            e.preventDefault();
+        },
+
+        render: function () {
+            return (
+                <div className="top-bar">
+                    <div className="top-bar-left">
+                        <ul className="menu">
+                            <li className="menu-text">React Weather app!</li>
+                            <li><IndexLink to="/">Weather</IndexLink></li>
+                            <li><Link to="/about">About</Link></li>
+                            <li><Link to="/more">More</Link></li>
+                        </ul>
+                    </div>
+                    <div className="top-bar-right">
+                        <form onSubmit={this.onSearch}>
+                            <ul className="menu">
+                                <li>
+                                    <input type="search" placeholder="Search Weather"/>
+                                </li>
+                                <li>
+                                    <input type="submit" className="button" value="Get weather"/>
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            );
+        }
+    }
 );
 
 module.exports = NavComponent;
